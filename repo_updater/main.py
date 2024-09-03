0a813ca9f6337a6db9d504a9ee648f0b0b3a45df
DarthApophis <albert_calinescu@yahoo.com>
2024-09-03 18:50:33 +0300

    Git RepoUpdater tool
    Added successful repo update teams notifications with newly added commits.
    Added support for install command options.
    Reduced default polling period for faster update of the config json during runtime.
    Added webhook config verification.

--- repo_updater/main.py
+++ repo_updater/main.py
@@ -17,7 +17,7 @@ import pymsteams
 NO_CHANGES = "Already up to date."
 DATA_FILE_NAME = "config_file.json"
 DIST_FOLDER = "dist"
-DEFAULT_POLLING_PERIOD = 3600  # seconds of wait time before each iteration
+DEFAULT_POLLING_PERIOD = 60  # seconds of wait time before each iteration
 
 
 @dataclass
@@ -29,6 +29,7 @@ class ConfigData:
     repo_location: str
     checkout_branch: str
     setup_options: str
+    install_options: str
     receiver_teams_webhook: str
     smtp_server: str
     smtp_port: str
@@ -124,9 +125,29 @@ def repo_update(config_data: ConfigData):
 
     rebase_result = git_pull_rebase(config_data)
 
-    if rebase_result == NO_CHANGES:
-        print(f"Repo '{config_data.repo_location}' is already up to date.")
+    if not rebase_result:
+        successfully_updated_msg_content = (
+            f"Repository '{config_data.repo_location}'"
+            f"::{config_data.checkout_branch} is already up to date.\n"
+        )
+        print(successfully_updated_msg_content)
         return
+
+    workstation_id = socket.gethostname()
+    new_commits_msg = "\nUpdated with the following commits:\n"
+    for idx, new_commit in enumerate(rebase_result):
+        new_commits_msg += (f"\nCommit #{idx+1}\n"
+                            f"Author: {new_commit.author}\n"
+                            f"Commit message:\n{new_commit.message}")
+
+    success_msg_title = "SUCCESSFUL RepoUpdater notification"
+    success_msg_content = (
+        f"RepoUpdater running on workstation: {workstation_id}.\n"
+        f"successfully updated repo:'{config_data.repo_location}'"
+        f"::{config_data.checkout_branch}\n"
+        f"{new_commits_msg}"
+        f"Next update scheduled in {config_data.polling_period/60:.2f} minutes."
+        )
 
     cleanup_dist_location(config_data)
 
@@ -134,8 +155,10 @@ def repo_update(config_data: ConfigData):
 
     pip_install_dist(config_data)
 
+    send_teams_webhook(success_msg_title, success_msg_content, config_data)
 
-def git_pull_rebase(config_data):
+
+def git_pull_rebase(config_data) -> list[git.Commit]:
     """
     Uses the GitPython library to perform a checkout and a git pull rebase operations.
 
@@ -146,17 +169,20 @@ def git_pull_rebase(config_data):
 
     """
     repo_git = git.Git(config_data.repo_location)
-    rebase_result = ""
+    repo = git.Repo(config_data.repo_location)
     try:
-        checkout_result = repo_git.execute(
+        repo_git.execute(
             f"git checkout {config_data.checkout_branch}")
-        rebase_result = repo_git.execute(
+        commits_behind = repo.iter_commits(
+            f'{config_data.checkout_branch}..origin/{config_data.checkout_branch}')
+        repo_git.execute(
             f"git pull --rebase origin {config_data.checkout_branch}")
+        print("GIT REBASE SUCCESSFUL")
     except Exception as ex:
         git_error_msg = (f"FAILED updating repo {config_data.repo_location} "
                          f"with exception: \n{ex}")
         raise Exception(git_error_msg)
-    return rebase_result
+    return list(commits_behind)
 
 
 def cleanup_dist_location(config_data):
@@ -190,7 +216,10 @@ def python_setup_sdist(config_data):
     Returns:
 
     """
-    setup_cmd = ["python", "setup.py", "sdist", "--release", "IDNOW_Release_8"]
+    setup_cmd = ["python",
+                 "setup.py",
+                 "sdist",
+                 *config_data.setup_options.split()]
     setup_process = subprocess.run(setup_cmd,
                                    shell=True,
                                    stdout=subprocess.PIPE,
@@ -214,7 +243,10 @@ def pip_install_dist(config_data):
 
     """
     dist_pckg = os.listdir(os.path.join(config_data.repo_location, DIST_FOLDER))[0]
-    install_cmd = ["pip", "install", f"{DIST_FOLDER}/{dist_pckg}"]
+    install_cmd = ["pip",
+                   "install",
+                   f"{DIST_FOLDER}/{dist_pckg}",
+                   *config_data.install_options.split()]
     install_process = subprocess.run(install_cmd,
                                      shell=True,
                                      stdout=subprocess.PIPE,
@@ -242,15 +274,15 @@ def send_email(msg_title, msg_content, config_data: ConfigData):
     try:
         receiver_data = config_data.receiver_email_list
         if not receiver_data:
-            print(f"Cannot send emails, receiver email not configured.\n"
-                  f"If you want email notifications, please configure the tool")
+            print("Cannot send emails, receiver email not configured.\n"
+                  "If you want email notifications, please configure the JSON file.")
             return
         emsg = email.message.EmailMessage()
         emsg['Subject'] = msg_title
         emsg['To'] = ", ".join(config_data.receiver_email_list)
         emsg['From'] = config_data.sender_email
         emsg.set_content(msg_content)
-        with smtplib.SMTP(config_data.smtp_server, config_data.smtp_port) as s:
+        with smtplib.SMTP(config_data.smtp_server, int(config_data.smtp_port)) as s:
             s.starttls()
             auth_result = s.login(config_data.user_name, config_data.user_password)
             send_result = s.send_message(emsg)
@@ -272,11 +304,11 @@ def send_teams_webhook(msg_title,msg_content, config_data: ConfigData):
     Returns:
         None
     """
-    if not config_data.receiver_teams_webhook:
-        print(f"Cannot send Teams messages, receiver webhook not configured.\n"
-              f"If you want Teams notifications, please configure the tool.")
-        return
     try:
+        if not config_data.receiver_teams_webhook:
+            print("Cannot send Teams messages, receiver Teams webhook not configured.\n"
+                  "If you want Teams notifications, please configure the JSON file.")
+            return
         t_msg = pymsteams.connectorcard(config_data.receiver_teams_webhook)
         msg_content = "<pre>"+"<br>".join(msg_content.splitlines())+ "</pre>"
         t_msg.title(msg_title)
